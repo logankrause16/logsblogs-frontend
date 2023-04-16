450d8af6e36bf3b0625b66c17844b9b6ae91f38d
Logan Krause <logankruase16@gmail.com>
2023-04-16 19:04:59 -0400

    Updating

--- src/app/black-series/black-series.component.ts
+++ src/app/black-series/black-series.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api.service';
 import { BlackSeries } from 'src/assets/message';
-import { DataGridComponent } from '../shared/data-grid/data-grid.component';
 
 @Component({
   selector: 'app-black-series',
